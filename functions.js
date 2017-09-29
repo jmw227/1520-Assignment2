332230f9ec9993095b64927113bfa9d29940c573
Jacob Winkler <jwinkler1313@gmail.com>
2017-09-29 13:51:33 -0400

    Almost There
    Fixed alerts and display issues where could not get a blank slate in between turns. Added in scoring, hit miss detection visualization, and some general fixes.

--- functions.js
+++ functions.js
@@ -132,8 +132,13 @@ function generateFullString(ship, length)
 }
 	
 function playerInfo()
-	{
+{
 		//player objects
+		if(localStorage.getItem('HighScores') == null)
+		{
+			var a = [0,0,0,0,0,0,0,0,0,0];
+			localStorage.setItem('HighScores', a);
+		}
 		var playerOne={name:'', placementString:''};
 		var playerTwo={name:'', placementString:''};
 		
@@ -145,68 +150,243 @@ function playerInfo()
 		playerTwo.placementString = prompt(playerTwo.name+', enter your Ship Placements: ','A:A1-A5;B:B1-B4;S:C1-C3');
 		playerTwo.placementString = placementTester(playerTwo);
 		//store names and placementstrings for both players
+		var sunk1 = [0,0,0];
+		var sunk2 = [0,0,0,];
 		sessionStorage.setItem('playOneName', playerOne.name);
 		sessionStorage.setItem('playOnePlace', playerOne.placementString);
+		sessionStorage.setItem('playOneHit', '');
+		sessionStorage.setItem('playOneMiss', '');
+		sessionStorage.setItem('playOneSunk', sunk1);
 		sessionStorage.setItem('playTwoName', playerTwo.name);
 		sessionStorage.setItem('playTwoPlace', playerTwo.placementString);
-		document.getElementById('playButton').style.display = 'none';
+		sessionStorage.setItem('playTwoHit', '');
+		sessionStorage.setItem('playTwoMiss', '');
+		sessionStorage.setItem('playTwoSunk', sunk2);
+		document.getElementById('playButton').style.visibility = 'hidden';
 		sessionStorage.setItem('currentPlayer', '2');
 		sessionStorage.setItem('playOneScore', 24);
 		sessionStorage.setItem('playTwoScore', 24);
+		
 		changeTurns();
 		//Begin Playing
 }
-function Play(clicked_id)
+async function Play(clicked_id)
 {
-	document.getElementById('topGrid').style.display = 'none';
-	document.getElementById('botGrid').style.display = 'none';
 	var swap = sessionStorage.getItem('currentPlayer');
 	var enemy;
+	var enemySunk;
+	var currentHit;
+	var currentMiss;
+	var currentSunk;
+	var currentName;
+	var alreadyGuessed = true;
+	var result;
 	switch(swap){
 	case '2':
 	
 		enemy = sessionStorage.getItem('playOnePlace');
+		enemySunk = sessionStorage.getItem('playOneSunk');
+		currentHit = sessionStorage.getItem('playTwoHit');
+		currentMiss = sessionStorage.getItem('playTwoMiss');
+		currentSunk = sessionStorage.getItem('playTwoSunk');
+		currentName = sessionStorage.getItem('playTwoName');
 
 		break;
 	case '1':
 	
 		enemy = sessionStorage.getItem('playTwoPlace');
+		enemySunk = sessionStorage.getItem('playTwoSunk');
+		currentHit = sessionStorage.getItem('playOneHit');
+		currentMiss = sessionStorage.getItem('playOneMiss');
+		currentSunk = sessionStorage.getItem('playOneSunk');
+		currentName = sessionStorage.getItem('playOneName');
+		
 
 		break;
 	}
-	
-	var currentID = clicked_id.replace(/T/, "");
-	document.write(currentID);
+	enemy = enemy.replace(/,/g,"");
+	enemySunk = enemySunk.replace(/,/g,"");
+	enemySunk = [parseInt(enemySunk[0]), parseInt(enemySunk[1]), parseInt(enemySunk[2])];
+	currentSunk = currentSunk.replace(/,/g,"");
+	currentSunk = parseInt(currentSunk[0])+parseInt(currentSunk[1])+ parseInt(currentSunk[2]);
+	var alreadyDead = [false,false,false];
+	if(enemySunk[0] == 5)
+		alreadyDead[0] = true;
+	if(enemySunk[1] == 4)
+		alreadyDead[1] = true;
+	if(enemySunk[2] == 3)
+		alreadyDead[2] = true;
+		var currentID = clicked_id.replace(/T/, "");
 	//FOR LOOP TO CHECK IF THE BOX CLICKED IS A HIT OR A MISS, ADD THE SESSIONSTORAGE FOR HIT/MISS TRACKING
-	for(i=0;i<enemy.length;i=i+2)
-	{
-		
-	}
-	
-	changeTurns();
-	
+		alreadyGuessed = false;
+		for(i=0;i<currentHit.length;i=i+2)
+		{
+			if(currentID == (currentHit[i]+currentHit[i+1]))
+			{
+				alreadyGuessed = true; 
+			}
+		}
+		for(i=0;i<currentMiss.length;i=i+2)
+		{
+			if(currentID == (currentMiss[i]+currentMiss[i+1]))
+			{
+				alreadyGuessed = true; 
+			}
+		}
+		if(alreadyGuessed)
+		{
+			alert("You Already Guessed this Spot, try again");
+		} 
+		else {
+			
+			for(i=0;i<enemy.length;i=i+2)
+			{
+				
+				if(currentID == (enemy[i]+enemy[i+1]))
+				{
+					result = true;
+					break;
+				} else 
+				{result = false;}
+			}
+				
+			if(result)
+			{
+				console.log(i);
+				if(i<10)
+				{
+					enemySunk[0]=enemySunk[0]+1;
+					console.log(enemySunk);
+				} else if(i<18)
+				{
+					enemySunk[1]++;
+				} else
+				{
+					enemySunk[2]++;
+				}
+				if(enemySunk[0] == 5 && !alreadyDead[0])
+				{
+					setTimeout(function(){alert("You Sunk their Aircraft Carrier")}, 100);
+				}else
+				if(enemySunk[1] == 4 && !alreadyDead[1])
+				{
+					setTimeout(function(){alert("You sunk their Battleship")}, 100);
+				}else
+				if(enemySunk[2] == 3 && !alreadyDead[2])
+				{
+					setTimeout(function(){alert("You sunk their submarine")}, 100);
+				}else{
+				setTimeout(function(){alert("It was a hit")}, 100);
+				}
+				currentHit = currentHit+currentID;
+			} else
+			{
+				setTimeout(function(){alert("It was a miss...")},100);
+				currentMiss = currentMiss+currentID;
+			}
+			if(enemySunk[0]+enemySunk[1]+enemySunk[2] == 12)
+			{
+				alert("Congratulations "+currentName+" You won with a score of "+(24-(2*currentSunk)));
+				addScore(24-currentSunk);
+				
+			}
+			switch(swap){
+			case '2':
+				sessionStorage.setItem('playTwoHit', currentHit);
+				sessionStorage.setItem('playTwoMiss', currentMiss);
+				sessionStorage.setItem('playOneSunk', enemySunk);
+				break;
+			case '1':
+				sessionStorage.setItem('playOneHit', currentHit);
+				sessionStorage.setItem('playOneMiss', currentMiss);
+				sessionStorage.setItem('playTwoSunk', enemySunk);
+				break;
+			}
+			
+			
+			changeTurns();
+		}
 }
-		
-function changeTurns()
+
+function addScore(score)
 {
+	var highs = localStorage.getItem('HighScores');
+	var highsArray = new Array(10);
+	highs = highs.replace(/,/g,'');
+	for(i=0;i<highsArray;i++)
+	{
+		highsArray[i]= parseInt(highs[i]);
+	}
+	highsArray.push(score);
+	highsArray.sort(numberSort);
+	highsArray.pop();
+	console.log(highsArray);
+}
+function numberSort(a,b)
+{
+	return a-b;
+}
+
+function blankIt()
+{
+	return Promise.resolve().then(function(){
+		setTimeout(function(){
+			document.getElementById('mainPlaySpace').style.visibility ='hidden'},0);}
+			);
+}
+
+function unBlankIt()
+{
+		return Promise.resolve().then(function(){
+		setTimeout(function(){
+			document.getElementById('mainPlaySpace').style.visibility ='visible'},200);}
+			
+			);
+}
+
+ async function changeTurns()
+{
+	await blankIt();
 	var swap = sessionStorage.getItem('currentPlayer');
 	var currentName;
 	var currentPlace;
+	var currentHit;
+	var currentMiss;
+	var enemyHit;
+	var enemyMiss;
+	
+	var blocks = document.getElementsByClassName('playBlock');
+	for(i=0;i<blocks.length;i++)
+	{
+		blocks[i].style.backgroundColor = 'cyan';
+	}
 	switch(swap){
 	case '2':
 		currentName = sessionStorage.getItem('playOneName');
 		currentPlace = sessionStorage.getItem('playOnePlace');
+		currentHit = sessionStorage.getItem('playOneHit');
+		currentMiss = sessionStorage.getItem('playOneMiss');
+		enemyHit = sessionStorage.getItem('playTwoHit');
+		enemyMiss = sessionStorage.getItem('playTwoMiss');
 		sessionStorage.setItem('currentPlayer', '1');
 		break;
 	case '1':
 		currentName = sessionStorage.getItem('playTwoName');
 		currentPlace = sessionStorage.getItem('playTwoPlace');
+		currentHit = sessionStorage.getItem('playTwoHit');
+		currentMiss = sessionStorage.getItem('playTwoMiss');
+		enemyHit = sessionStorage.getItem('playOneHit');
+		enemyMiss = sessionStorage.getItem('playOneMiss');
 		sessionStorage.setItem('currentPlayer','2');
 		break;
 	}
-	alert('It is '+currentName+'\'s Turn');
-	document.getElementById('topGrid').style.display = 'block';
-	document.getElementById('botGrid').style.display = 'block';
+			
+
+			
+			setTimeout(function(){alert("It is "+currentName+"\'s Turn")},100);
+			await unBlankIt();
+			
+			
 	var locID;
 	var a;
 	currentPlace = currentPlace.split(',');
@@ -224,6 +404,26 @@ function changeTurns()
 				document.getElementById(locID).innerHTML = 'S';
 	
 	}
-	
-	
+	for(i=0;i<currentHit.length;i=i+2)
+	{
+		locID = 'T'+currentHit[i]+currentHit[i+1];
+		document.getElementById(locID).style.backgroundColor = 'red';
+	}
+	for(i=0;i<currentMiss.length;i=i+2)
+	{
+		locID = 'T'+currentMiss[i]+currentMiss[i+1];
+		document.getElementById(locID).style.backgroundColor = 'white';
+	}
+	for(i=0;i<enemyHit.length;i=i+2)
+	{
+		locID = 'Bot'+enemyHit[i]+enemyHit[i+1];
+		document.getElementById(locID).style.backgroundColor = 'red';
+	}
+	for(i=0;i<enemyMiss.length;i=i+2)
+	{
+		locID = 'Bot'+enemyMiss[i]+enemyMiss[i+1];
+		document.getElementById(locID).style.backgroundColor = 'white';
+	}
+				
+				
 }
